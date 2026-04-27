834741287bfc4d705abe6ab274c3d7b471fd1dd8
root <root@DESKTOP-NUC73FD.localdomain>
2026-04-27 21:54:59 +0100

    Update

--- src/utils/utils_quotes.c
+++ src/utils/utils_quotes.c
@@ -11,32 +11,12 @@ static char	*alloc_remove_quotes(const char *str, int *len)
 	return (result);
 }
 
-static int	handle_quote_remove(const char *str, int *i, char *in_quote,
-		char *quote_char)
-{
-	if (!*in_quote && (str[*i] == '\'' || str[*i] == '"'))
-	{
-		*in_quote = 1;
-		*quote_char = str[*i];
-		(*i)++;
-		return (1);
-	}
-	if (*in_quote && str[*i] == *quote_char)
-	{
-		*in_quote = 0;
-		(*i)++;
-		return (1);
-	}
-	return (0);
-}
-
 char	*remove_quotes(const char *str)
 {
 	char	*result;
 	int		len;
 	int		i;
 	int		j;
-	char	in_quote;
 	char	quote_char;
 
 	result = alloc_remove_quotes(str, &len);
@@ -44,12 +24,21 @@ char	*remove_quotes(const char *str)
 		return (NULL);
 	i = 0;
 	j = 0;
-	in_quote = 0;
 	quote_char = 0;
 	while (i < len)
 	{
-		if (handle_quote_remove(str, &i, &in_quote, &quote_char))
+		if (!quote_char && (str[i] == '\'' || str[i] == '"'))
+		{
+			quote_char = str[i];
+			i++;
 			continue ;
+		}
+		if (quote_char && str[i] == quote_char)
+		{
+			quote_char = 0;
+			i++;
+			continue ;
+		}
 		result[j++] = str[i++];
 	}
 	result[j] = '\0';
